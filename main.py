e34c3be963521cdd33a401cbcbd2ffafbeced8a9
iskra <iskrag@gmail.com>
2024-05-20 21:14:46 +0200

    Add app descriptions, image, and links

--- main.py
+++ main.py
@@ -16,14 +16,21 @@ with col2:
 content2 = 'Below you can find some of the apps I have built in Python. Feel free to contact me!'
 sl.write(content2)
 
-col3, col4 = sl.columns(2)
+col3, empty_col, col4 = sl.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv('data.csv', sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         sl.header(row['title'])
+        sl.write(row['description'])
+        sl.image('images/' + row['image'])
+        sl.write(f"[SourceCode]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         sl.header(row['title'])
+        sl.write(row['description'])
+        sl.image('images/'+row['image'])
+        sl.write(f"[SourceCode]({row['url']})")
